ae92a5979943770cd19d9f45dbe7efcfdf09cd64
Rockie Guo <rockie@kitmi.com.au>
2020-07-08 15:10:55 +1000

    Update Errors.js

--- src/utils/Errors.js
+++ src/utils/Errors.js
@@ -6,7 +6,7 @@
  */
 
 const { 
-    Errors: { GeneralError, ApplicationError, InvalidArgument, InvalidConfiguration, ExposableError }
+    Errors: { GeneralError, InvalidArgument, InvalidConfiguration, ExposableError }
 } = require('@genx/app');
 
 const HttpCode = require('http-status-codes');
@@ -18,7 +18,7 @@ const HttpCode = require('http-status-codes');
  */
 class BadRequest extends ExposableError {
     constructor(message, info) {
-        super(message, info, HttpCode.BAD_REQUEST, 'E_BAD_REQ');
+        super(message, info, HttpCode.BAD_REQUEST, 'E_BAD_REQUEST');
     }
 }  
    
@@ -37,10 +37,30 @@ class NotFound extends ExposableError {
  * Http Unauthorized, 401.
  * @class 
  * @extends ExposableError
+ * 
+ * 401 UNAUTHORIZED vs 403 FORBIDDEN
+ * 401 It’s for authentication, not authorization. 
+ * Receiving a 401 response is the server telling you, “you aren’t authenticated–either not authenticated at all or authenticated incorrectly–but please reauthenticate and try again.” 
+ * To help you out, it will always include a WWW-Authenticate header that describes how to authenticate.
  */
 class Unauthorized extends ExposableError {
     constructor(message, info) {
-        super(message, info, HttpCode.UNAUTHORIZED, 'E_UNAUTHORIZED');
+        super(message, info, HttpCode.UNAUTHORIZED, 'E_UNAUTHENTICATED');
+    } 
+} 
+
+/**
+ * Http Forbidden, 403.
+ * @class 
+ * @extends ExposableError
+ * 
+ * 401 UNAUTHORIZED vs 403 FORBIDDEN
+ * Receiving a 403 response is the server telling you, “I’m sorry. I know who you are–I believe who you say you are–but you just don’t have permission to access this resource. Maybe if you ask the system administrator nicely, you’ll get permission. But please don’t bother me again until your predicament changes.”
+ * In summary, a 401 Unauthorized response should be used for missing or bad authentication, and a 403 Forbidden response should be used afterwards, when the user is authenticated but isn’t authorized to perform the requested operation on the given resource.
+ */
+class Forbidden extends ExposableError {
+    constructor(message, info) {
+        super(message, info, HttpCode.FORBIDDEN, 'E_FORBIDDEN');
     } 
 } 
 
@@ -58,20 +78,27 @@ class ServiceUnavailable extends GeneralError {
 /**
  * Internal server error, 500
  */
-class ServerError extends ApplicationError {
+class ServerError extends GeneralError {
+    constructor(message, info) {
+        super(message, info, HttpCode.INTERNAL_SERVER_ERROR, 'E_SERVER');
+    }
 }
-
 /**
  * Internal server error, 500
  */
-class ExternalServiceError extends ApplicationError {
+class ExternalServiceError extends GeneralError {
+    constructor(message, info) {
+        super(message, info, HttpCode.INTERNAL_SERVER_ERROR, 'E_EXTERNAL');
+    }
 }
 
 exports.InvalidArgument = InvalidArgument;
 exports.InvalidConfiguration = InvalidConfiguration;
 exports.BadRequest = BadRequest;
 exports.NotFound = NotFound;
-exports.Unauthorized = Unauthorized;
+exports.Unauthorized = Unauthorized; 
+exports.Unauthenticated = Unauthorized; // try use Unauthenticated instead of Unauthorized for better expressing the error in code
+exports.Forbidden = Forbidden;
 exports.ServerError = ServerError;
 exports.ExternalServiceError = ExternalServiceError;
 exports.ServiceUnavailable = ServiceUnavailable;
